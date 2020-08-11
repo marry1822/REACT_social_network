41e63527799dd261aa2fa7684322fafb8a324e72
marry1822 <marry18222@gmail.com>
2020-08-11 13:43:57 +0200

    store added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import * as serviceWorker from "./serviceWorker";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { addPost } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
-import { updateNewPostText } from "./redux/state";
 
 let rerenderEntireTree = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
 			<App
 				state={state}
-				addPost={addPost}
-				updateNewPostText={updateNewPostText}
+				addPost={store.addPost.bind(store)}
+				updateNewPostText={store.updateNewPostText.bind(store)}
 			/>
 		</BrowserRouter>,
 		document.getElementById("root")
 	);
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,96 +1,100 @@
-let rerenderEntireTree = () => {};
-
-let state = {
-	profilePage: {
-		posts: [
-			{ id: 1, message: "Hi, how are you?", likeCounts: 12 },
-			{ id: 2, message: "It's my first post!", likeCounts: 5 },
-		],
-		newPostText: "new post text",
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: "Hi, how are you?", likeCounts: 12 },
+				{ id: 2, message: "It's my first post!", likeCounts: 5 },
+			],
+			newPostText: "new post text",
+		},
+		dialogsPage: {
+			dialogs: [
+				{
+					id: 1,
+					name: "Masha",
+					avatarSrc:
+						"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
+				},
+				{
+					id: 2,
+					name: "Misha",
+					avatarSrc:
+						"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
+				},
+				{
+					id: 3,
+					name: "Dasha",
+					avatarSrc:
+						"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
+				},
+				{
+					id: 4,
+					name: "Sasha",
+					avatarSrc:
+						"https://static.tildacdn.com/tild3966-6236-4137-b163-633865346433/flat-faces-icons-cir.png",
+				},
+				{
+					id: 5,
+					name: "Pasha",
+					avatarSrc:
+						"https://thumb.tildacdn.com/tild6464-6261-4662-b038-333831326134/-/resize/160x/-/format/webp/man1.png",
+				},
+			],
+			messages: [
+				{ id: 1, message: "Hi!" },
+				{ id: 2, message: "How are you?" },
+				{ id: 3, message: "What's your name?" },
+				{ id: 4, message: "How's the weather?" },
+				{ id: 5, message: "How old are you?" },
+			],
+		},
+		sideBar: {
+			friends: [
+				{
+					id: 1,
+					name: "Masha",
+					avatarSrc:
+						"https://lh3.googleusercontent.com/proxy/OugIn_p8_BiOQBxKt_oVBTZ4lw7-RclQAViNOtdcAgWUS4-LozQFzq8o81-sLR4wl2QeQQtpo5VRf3Nhv6WZhE5J56NP30_DCKi_I4cquxqa1NuIXKpJKtxgcZuwuMTSJ8HuFL-zpGWTGy8",
+				},
+				{
+					id: 2,
+					name: "Misha",
+					avatarSrc:
+						"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
+				},
+				{
+					id: 3,
+					name: "Dasha",
+					avatarSrc:
+						"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
+				},
+			],
+		},
 	},
-	dialogsPage: {
-		dialogs: [
-			{
-				id: 1,
-				name: "Masha",
-				avatarSrc:
-					"https://lh3.googleusercontent.com/proxy/OugIn_p8_BiOQBxKt_oVBTZ4lw7-RclQAViNOtdcAgWUS4-LozQFzq8o81-sLR4wl2QeQQtpo5VRf3Nhv6WZhE5J56NP30_DCKi_I4cquxqa1NuIXKpJKtxgcZuwuMTSJ8HuFL-zpGWTGy8",
-			},
-			{
-				id: 2,
-				name: "Misha",
-				avatarSrc:
-					"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
-			},
-			{
-				id: 3,
-				name: "Dasha",
-				avatarSrc:
-					"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
-			},
-			{
-				id: 4,
-				name: "Sasha",
-				avatarSrc:
-					"https://static.tildacdn.com/tild3966-6236-4137-b163-633865346433/flat-faces-icons-cir.png",
-			},
-			{
-				id: 5,
-				name: "Pasha",
-				avatarSrc:
-					"https://thumb.tildacdn.com/tild6464-6261-4662-b038-333831326134/-/resize/160x/-/format/webp/man1.png",
-			},
-		],
-		messages: [
-			{ id: 1, message: "Hi!" },
-			{ id: 2, message: "How are you?" },
-			{ id: 3, message: "What's your name?" },
-			{ id: 4, message: "How's the weather?" },
-			{ id: 5, message: "How old are you?" },
-		],
+	getState() {
+		return this._state;
 	},
-	sideBar: {
-		friends: [
-			{
-				id: 1,
-				name: "Masha",
-				avatarSrc:
-					"https://lh3.googleusercontent.com/proxy/OugIn_p8_BiOQBxKt_oVBTZ4lw7-RclQAViNOtdcAgWUS4-LozQFzq8o81-sLR4wl2QeQQtpo5VRf3Nhv6WZhE5J56NP30_DCKi_I4cquxqa1NuIXKpJKtxgcZuwuMTSJ8HuFL-zpGWTGy8",
-			},
-			{
-				id: 2,
-				name: "Misha",
-				avatarSrc:
-					"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
-			},
-			{
-				id: 3,
-				name: "Dasha",
-				avatarSrc:
-					"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
-			},
-		],
+	_callSubscriber() {
+		console.log("State changed");
+	},
+	addPost() {
+		let newPost = {
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			likeCounts: 0,
+		};
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = "";
+		this._callSubscriber(this._state);
+	},
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
 	},
 };
 
-export const addPost = () => {
-	let newPost = {
-		id: 5,
-		message: state.profilePage.newPostText,
-		likeCounts: 0,
-	};
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = "";
-	rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText;
-	rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
