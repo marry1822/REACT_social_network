3f0001de221534c0b817504070fcbdbbcfd8b91d
marry1822 <marry18222@gmail.com>
2020-08-13 13:02:02 +0200

    redux installed, new store is created

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
+import store from "./redux/reduxStore";
 import { BrowserRouter } from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App
-				state={state}
-				dispatch={store.dispatch.bind(store)}
-			/>
+			<App state={state} dispatch={store.dispatch.bind(store)} />
 		</BrowserRouter>,
 		document.getElementById("root")
 	);
@@ -20,7 +17,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+	let state = store.getState()
+	rerenderEntireTree(state);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,7 +1,49 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+	dialogs: [
+		{
+			id: 1,
+			name: "Masha",
+			avatarSrc:
+				"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
+		},
+		{
+			id: 2,
+			name: "Misha",
+			avatarSrc:
+				"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
+		},
+		{
+			id: 3,
+			name: "Dasha",
+			avatarSrc:
+				"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
+		},
+		{
+			id: 4,
+			name: "Sasha",
+			avatarSrc:
+				"https://static.tildacdn.com/tild3966-6236-4137-b163-633865346433/flat-faces-icons-cir.png",
+		},
+		{
+			id: 5,
+			name: "Pasha",
+			avatarSrc:
+				"https://thumb.tildacdn.com/tild6464-6261-4662-b038-333831326134/-/resize/160x/-/format/webp/man1.png",
+		},
+	],
+	messages: [
+		{ id: 1, message: "Hi!" },
+		{ id: 2, message: "How are you?" },
+		{ id: 3, message: "What's your name?" },
+		{ id: 4, message: "How's the weather?" },
+		{ id: 5, message: "How old are you?" },
+	],
+	newMessageText: "",
+}
+const dialogsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_MESSAGE:
 			let newMessage = {
