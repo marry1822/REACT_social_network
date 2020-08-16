e25c13876701592c0b0afe4763e1322a9f7a7fbe
marry1822 <marry18222@gmail.com>
2020-08-16 17:41:53 +0200

    API used

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,25 @@
 import React from "react";
 import classes from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
 	if (props.users.length === 0) {
-    
-		props.setUsers([
-			{
-				id: 1,
-				avatarUrl:
-					"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
-				followed: false,
-				fullName: "Masha",
-				status: "Bored...",
-				location: {
-					cityName: "Moscow",
-					country: "Russia",
-				},
-			},
-			{
-				id: 2,
-				avatarUrl:
-					"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png",
-				followed: false,
-				fullName: "Misha",
-				status: "I'm happy!",
-				location: {
-					cityName: "New-York",
-					country: "USA",
-				},
-			},
-			{
-				id: 3,
-				avatarUrl:
-					"https://fotograf-kuchin.ru/wp-content/uploads/2017/06/people.png",
-				followed: true,
-				fullName: "Dasha",
-				status: "Looking for a job",
-				location: {
-					cityName: "London",
-					country: "UK",
-				},
-			},
-			{
-				id: 4,
-				avatarUrl:
-					"https://static.tildacdn.com/tild3966-6236-4137-b163-633865346433/flat-faces-icons-cir.png",
-				followed: true,
-				fullName: "Sasha",
-				status: "Watching a movie",
-				location: {
-					cityName: "Mexico",
-					country: "Mexico",
-				},
-			},
-		]);
+		axios
+			.get("https://social-network.samuraijs.com/api/1.0/users")
+			.then((response) => {
+				props.setUsers(response.data.items);
+			});
 	}
 
 	return props.users.map((user) => (
 		<div className={classes.user} key={user.id}>
 			<div className={classes.userAvatar}>
 				<div>
-					<img src={user.avatarUrl} alt="" />
+					<img
+						src={user.photos.small != null ? user.photos.small : userPhoto}
+						alt=""
+					/>
 				</div>
 				<div>
 					{user.followed ? (
@@ -84,12 +43,12 @@ let Users = (props) => {
 			</div>
 			<div className={classes.userInfo}>
 				<div>
-					<div className={classes.fullName}>{user.fullName}</div>
+					<div className={classes.fullName}>{user.name}</div>
 					<div className={classes.status}>{user.status}</div>
 				</div>
 				<div className={classes.location}>
-					<div>{user.location.country}</div>
-					<div>{user.location.cityName}</div>
+					<div>{"user.location.country"}</div>
+					<div>{"user.location.cityName"}</div>
 				</div>
 			</div>
 		</div>
