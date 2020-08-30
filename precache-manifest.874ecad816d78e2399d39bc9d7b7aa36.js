c15e8310b0e72ba010df53d43787b7a42a2cb91c
marry1822 <marry18222@gmail.com>
2020-08-30 11:08:33 +0200

    Updates

---
+++ precache-manifest.874ecad816d78e2399d39bc9d7b7aa36.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b99d0d7d4b6e4b0e1fc194fe7f48766",
+    "url": "/REACT_social_network/index.html"
+  },
+  {
+    "revision": "10e814c91ce79de881f6",
+    "url": "/REACT_social_network/static/css/3.ac8e8504.chunk.css"
+  },
+  {
+    "revision": "fab4cf71969ecd15c4f9",
+    "url": "/REACT_social_network/static/css/4.24eccb26.chunk.css"
+  },
+  {
+    "revision": "c68613bc775269b0f61c",
+    "url": "/REACT_social_network/static/css/main.26c9986a.chunk.css"
+  },
+  {
+    "revision": "d6ea90035a55e64bbeeb",
+    "url": "/REACT_social_network/static/js/2.328fcf8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACT_social_network/static/js/2.328fcf8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e814c91ce79de881f6",
+    "url": "/REACT_social_network/static/js/3.911dcc24.chunk.js"
+  },
+  {
+    "revision": "fab4cf71969ecd15c4f9",
+    "url": "/REACT_social_network/static/js/4.594682a3.chunk.js"
+  },
+  {
+    "revision": "c68613bc775269b0f61c",
+    "url": "/REACT_social_network/static/js/main.cd6f4df8.chunk.js"
+  },
+  {
+    "revision": "3eaf23023c7a0620e6e0",
+    "url": "/REACT_social_network/static/js/runtime-main.c0b48852.js"
+  },
+  {
+    "revision": "f4f3dd7acfd64b16aa51027159a6f5e8",
+    "url": "/REACT_social_network/static/media/user.f4f3dd7a.png"
+  }
+]);
